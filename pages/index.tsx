403b53c176a108b3d754c222c620ac3734769735
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-11 19:40:40 -0300

    WIP home page

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Wrapper } from '../components';
+
+const Home: React.ReactNode = (): React.ReactNode => (
+  <Wrapper>
+    <h1>Test home page</h1>
+  </Wrapper>
+);
+
+export default Home;
