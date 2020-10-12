6effacefcaeca97c4fd645c26539f44318798010
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-12 01:56:24 -0300

    Create theme

---
+++ utils/palette.ts
@@ -0,0 +1,28 @@
+const dark = {
+  background: {
+    main: '#333'
+  },
+  text: {
+    primary: '#fff'
+  },
+  hightlight: {
+    error: '#ff0000',
+  }
+};
+
+const light = {
+  background: {
+    main: '#fff'
+  },
+  text: {
+    primary: '#333'
+  },
+  hightlight: {
+    error: '#ff0000'
+  }
+};
+
+export default {
+  dark,
+  light,
+};
