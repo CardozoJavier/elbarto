f83cbbd6984f4ad1a501d4eccd73c4c39eaf7205
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-12 21:55:37 -0300

    Fix wrapper styles

--- components/styles/Wrapper.styles.tsx
+++ components/styles/Wrapper.styles.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const WrapperContainer = styled.div`
   padding: 25px;
-
+  transition: background-color .5s;
+  background-color: ${({ theme: { background } }) => background.main};
   border: 1px solid magenta;
 `;
