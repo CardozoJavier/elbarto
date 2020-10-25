eab67e2778767e7b0c9b6ff9b7963f336139aaf8
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-25 04:40:32 -0300

    Add Wrapper styles in custom app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
 import Provider from '../Provider';
+import { Wrapper } from '../components';
 import '../styles/global.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <Provider>
-      <Component {...pageProps} />
+      <Wrapper>
+        <Component {...pageProps} />
+      </Wrapper>
     </Provider>
   );
 }
