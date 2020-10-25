d7ee22c85ceda32baff564cca3b6f696aa4b77cf
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-25 04:41:10 -0300

    Add constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,5 +7,17 @@ export const HEADER = 'header';
 export const TOGGLE_STATUS = 'toggle_status';
 export const LINK = 'link';
 export const IMAGE = 'image';
-export const FIRST_SECTION = 'first-section';
-export const SECOND_SECTION = 'second-section';
+export const BUTTON = 'button';
+export const CHECKOUT_PORTFOLIO = 'Check out portfolio';
+export const ONLY_PREV = 'only-prev';
+export const ONLY_NEXT = 'only-next';
+export const PROFILE_IMAGE = 'profile image';
+
+export const HOME = 'Home';
+export const ABOUT_ME = 'About me';
+export const PORTFOLIO = 'Portfolio';
+
+export const HREF_HOME = '/';
+export const HREF_ABOUT = '/about';
+export const HREF_PORTFOLIO = '/portfolio';
+export const HREF_CONTACT = '/contact';
