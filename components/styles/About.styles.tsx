9799ccc89b40f614c7b2120d2048bc500de65cda
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-25 18:09:03 -0300

    Update About styles

--- components/styles/About.styles.tsx
+++ components/styles/About.styles.tsx
@@ -25,11 +25,17 @@ export const Header = styled.h2`
   font-size: 3vw;
   color: ${({ theme }) => theme.white.w08};
   max-width: 30vw;
+  padding: 2vw;
+  border-radius: 3px;
+  background-color: ${({ theme }) => theme.black.b05};
 `;
 
 export const Description = styled.p`
-  color: ${({ theme }) => theme.white.w08};
+  color: ${({ theme }) => theme.black.b08};
+  background-color: ${({ theme }) => theme.white.w65};
   letter-spacing: 1px;
   margin: 5vh 0;
-  max-width: 25vw;
+  border-radius: 3px;
+  padding: 2vh 2vw;
+  font-weight: 600;
 `;
