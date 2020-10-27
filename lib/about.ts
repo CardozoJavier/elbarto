5b99d910c0b8d5513bdf156ecfe892d85d509f2b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-10-26 22:04:48 -0300

    Modify description about

--- lib/about.ts
+++ lib/about.ts
@@ -1,8 +1,7 @@
 const about = {
   title: 'My passion and focus is front-end development',
-  description: `I am a front-end developer with 2 years of experience building and testing web applications using the MERN stack.<br>
-  I am a passionate about technology and programming things. 
-  Create user interfaces that allow people to connect with machines is a concept that blew my mind.`,
+  description: `Create user interfaces that allow people to connect with machines is a concept that blew my mind.<br>
+  I am a front-end developer with 2 years of experience building and testing web applications using the MERN stack.`,
 };
 
 export default about;
