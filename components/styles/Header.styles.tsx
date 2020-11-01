0fe1ec652e866fd0398e56ce930f61b281be556d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-01 17:38:11 -0300

    Update mobile styles in Header section

--- components/styles/Header.styles.tsx
+++ components/styles/Header.styles.tsx
@@ -49,4 +49,86 @@ export const Item = styled.a`
     color: ${({ theme }) => theme.orange.o100};
     cursor: pointer;
   }
+
+  &.menu-closed {
+    color: ${({ theme }) => theme.white.w08};
+    opacity: 0;
+    transition: opacity .5s;
+  }
+
+  &.menu-openned {
+    color: ${({ theme }) => theme.white.w08};
+  }
+`;
+
+export const MobileHeaderContainer = styled.div``;
+
+export const MenuButtonContainer = styled.div`
+  position: absolute;
+  z-index: 2;
+  padding: 5px;
+  transition: transform 1s;
+  
+  &.menu-closed {
+    transform: translateX(0);
+  }
+  
+  &.menu-openned {
+    transform: translateX(calc(100vw - 83px));
+  }
+`;
+
+export const MenuButtonLine = styled.div`
+  width: 25px;
+  height: 3px;
+  margin: 3px 0;
+
+  &.menu-closed {
+    transition: transform 1s, opacity .5s;
+    background-color: #202020;
+
+    &.top-line, &.middle-line, &.bottom-line {
+      transform: rotate(0) translate(0, 0);
+      opacity: 1;
+    }
+  }
+  &.menu-openned {
+    transition: transform 1s, opacity 1.5s;
+    background-color: ${({ theme }) => theme.white.w08};
+
+    &.top-line {
+      transform: rotate(45deg) translate(5px, 4px);
+    }
+    &.middle-line {
+      transform: rotate(675deg);
+      opacity: 0;
+    }
+    &.bottom-line {
+      transform: rotate(-45deg) translate(4px, -4px);
+    }
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+  padding: 75px 10px;
+  z-index: 1;
+  align-items: flex-end;
+
+  &.menu-openned {
+    transition: width 1s, background-color 1s;
+    background-color: ${({ theme }) => theme.black.b08};
+  }
+
+  &.menu-closed {
+    transition: width 1s, background-color 1s;
+    width: 75px;
+    overflow: hidden;
+  }
 `;
