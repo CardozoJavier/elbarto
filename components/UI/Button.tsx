4cb60911805815a16ec68979fe78e9f04b3de182
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-09 00:19:30 -0300

    Update Button component

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -2,13 +2,11 @@ import {
   Container,
   Label,
 } from './styles/Button.styles';
-import getIcon from '../../icons/getIcon';
 import classes from '../../utils/classes';
 
 const Button = ({ label, type, icon, onClick }: ButtonProps): React.ReactElement => (
   <Container className={classes.get(type)} onClick={onClick}>
     <Label>{label}</Label>
-    {icon ? getIcon(icon) : null}
   </Container>
 );
 
