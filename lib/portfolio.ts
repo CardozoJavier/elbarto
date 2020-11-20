6a554582233f67c568b7df42d320d9fc610a6d35
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-19 22:13:59 -0300

    Create portfolio data

---
+++ lib/portfolio.ts
@@ -0,0 +1,20 @@
+const portfolio = {
+  title: 'My frontend portfolio and more',
+  description: 'Take a look to some of the projects I built for end clients or when I wanted to test others technologies.',
+  projects: [
+    {
+      title: 'UI library',
+      description: 'It\'s a react components library built on storybook.',
+      image: '/images/ui-kit.jpeg',
+      sections: [
+        {
+          title: 'UI library',
+          description: 'The client was hurry to built a components library with top technologies that allow they to extends and re-use easily. I decided to use storybook to show them the progress. On the other hand, I mainly used react and styled-components. I dockerize the app to ease the deploy job to the client\'s developers',
+          image: '',
+        }
+      ]
+    }
+  ]
+};
+
+export default portfolio;
