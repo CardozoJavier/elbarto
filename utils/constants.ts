4667278d17b2f738a06050cfe948ce7cbbb32536
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-22 17:08:31 -0300

    Order constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,70 +1,88 @@
-export const LINK = 'link';
-export const DARK = 'dark';
-export const LIGHT = 'light';
-export const IMAGE = 'image';
-export const HEADER = 'header';
-export const BUTTON = 'button';
-export const TOGGLE_ON = 'toggle_on';
-export const TOGGLE_OFF = 'toggle_off';
+/**
+ * Images names
+ */
 export const ARROW_ICON = 'arrow-icon';
 export const HEADER_ICON = 'header-icon';
-export const CONTACT_LINK = 'contact-link';
 export const PROFILE_IMAGE = 'profile image';
-export const TOGGLE_STATUS = 'toggle_status';
-export const CHECKOUT_PORTFOLIO = 'Check out portfolio';
 
-export const ONLY_PREV = 'only-prev';
-export const ONLY_NEXT = 'only-next';
 
-export const DEFAULT_ANIMATION = 'default-animation';
-export const LEFT_TO_RIGHT_ANIMATION = 'left-to-right-animation';
-export const RIGHT_TO_LEFT_ANIMATION = 'right-to-left-animation';
+/**
+ * Types
+ */
+export const LINK = 'link';
+export const BUTTON = 'button';
+export const MOBILE = 'mobile';
+export const TABLET = 'tablet';
+export const DESKTOP = 'desktop';
 
-export const PREV_ARROW = 'prev-arrow';
-export const NEXT_ARROW = 'next-arrow';
-
-export const NEXT_ACTION = 'next-action';
-export const PREV_ACTION = 'prev-action';
-
-export const HOME = 'Home';
-export const RESUME = 'Resume';
-export const ABOUT = 'About';
-export const PORTFOLIO = 'Portfolio';
-
+/**
+ * Link pages
+ */
 export const HREF_HOME = '/';
 export const HREF_ABOUT = '/about';
 export const HREF_RESUME = '/resume';
 export const HREF_CONTACT = '/contact';
 export const HREF_PORTFOLIO = '/portfolio';
+export const INFO = 'info';
 
-export const MOBILE = 'mobile';
-export const TABLET = 'tablet';
-export const DESKTOP = 'desktop';
-
+/**
+ * State actions
+ */
+export const DARK = 'dark';
+export const LIGHT = 'light';
+export const TOGGLE_ON = 'toggle_on';
+export const ONLY_PREV = 'only-prev';
+export const ONLY_NEXT = 'only-next';
+export const TOGGLE_OFF = 'toggle_off';
+export const PREV_ARROW = 'prev-arrow';
+export const NEXT_ARROW = 'next-arrow';
+export const NEXT_ACTION = 'next-action';
+export const PREV_ACTION = 'prev-action';
 export const MENU_CLOSED = 'menu-closed';
 export const MENU_OPENNED = 'menu-openned';
+export const TOGGLE_STATUS = 'toggle_status';
 
+/**
+ * Wordings
+ */
+export const HOME = 'Home';
+export const ABOUT = 'About';
+export const RESUME = 'Resume';
+export const PROJECT = 'project';
+export const PROJECTS = 'projects';
+export const PORTFOLIO = 'Portfolio';
+export const TAKE_A_LOOK = 'take a look';
+export const LETS_DEEP_DIVE = 'lets deep dive';
+export const DOWNLOAD_RESUME = 'Download resume';
+export const CHECKOUT_PORTFOLIO = 'Check out portfolio';
+
+
+/**
+ * CSS classes
+ */
+export const BOLD = 'bold';
 export const LOUD = 'loud';
 export const GHOST = 'ghost';
-export const BUTTON_SOLID = 'button-solid';
-export const BUTTON_LINE = 'button-line';
-
-export const SECOND_RESUME_SECTION = 'second-resume-section';
-
-export const CUSTOM_TITLE = 'custom-title';
-export const CUSTOM_SUBTITLE = 'custom-subtitle';
-export const CUSTOM_LIST_ITEM = 'custom-list-item';
-export const CUSTOM_PADDING = 'custom-padding';
-export const CUSTOM_PORTFOLIO_LINK = 'custom-portfolio-link';
-export const SECTION_TITLE = 'section-title';
-
-export const DOWNLOAD_RESUME = 'Download resume';
-
-export const MARGIN_20 = 'margin-20';
-
-export const ARROW_BUTTON = 'arrow-button';
+export const IMAGE = 'image';
 export const HIGHLIGHT = 'highlight';
-
-export const PROJECTS = 'projects';
-export const TAKE_A_LOOK = 'take a look';
+export const MARGIN_20 = 'margin-20';
+export const BUTTON_LINE = 'button-line';
+export const MAX_WIDTH_50 = 'max-width-50';
+export const CONTACT_LINK = 'contact-link';
+export const CUSTOM_TITLE = 'custom-title';
+export const ARROW_BUTTON = 'arrow-button';
+export const BUTTON_SOLID = 'button-solid';
+export const SECTION_TITLE = 'section-title';
+export const CUSTOM_PADDING = 'custom-padding';
+export const CUSTOM_SUBTITLE = 'custom-subtitle';
+export const CUSTOM_SECTION_1 = 'custom-section-1';
+export const CUSTOM_LIST_ITEM = 'custom-list-item';
+export const DEFAULT_ANIMATION = 'default-animation';
+export const CUSTOM_SECTION_IMG = 'custom-section-img';
+export const CUSTOM_PROJECT_LINK = 'custom-project-link';
+export const SECOND_RESUME_SECTION = 'second-resume-section';
+export const CUSTOM_PORTFOLIO_LINK = 'custom-portfolio-link';
+export const LEFT_TO_RIGHT_ANIMATION = 'left-to-right-animation';
+export const RIGHT_TO_LEFT_ANIMATION = 'right-to-left-animation';
 export const SECOND_PORTFOLIO_SECTION = 'second-portfolio-section';
+export const CUSTOM_DESCRIPTION_CONTAINER = 'custom-description-container';
