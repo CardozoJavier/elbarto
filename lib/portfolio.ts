78af689e596752bdc5277776e132098b93676341
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-22 17:10:01 -0300

    Add portfolio data

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -1,16 +1,35 @@
+import {
+  CUSTOM_SECTION_1,
+} from '../utils/constants';
+
 const portfolio = {
   title: 'My frontend portfolio and more',
   description: 'Take a look to some of the projects I built for end clients or when I wanted to test others technologies.',
   projects: [
     {
+      id: 'ui-library',
       title: 'UI library',
       description: 'It\'s a react components library built on storybook.',
-      image: '/images/ui-kit.jpeg',
+      image: '/images/ui-kit-thumbnail.png',
+      alt: 'ui kit image',
+      technologies: {
+        title: 'Technologies used:',
+        list: ['React.js', 'Storybook', 'Docker', 'Webpack'],
+      },
       sections: [
         {
-          title: 'UI library',
-          description: 'The client was hurry to built a components library with top technologies that allow they to extends and re-use easily. I decided to use storybook to show them the progress. On the other hand, I mainly used react and styled-components. I dockerize the app to ease the deploy job to the client\'s developers',
-          image: '',
+          title: 'What the client needed?',
+          description: `A react components library that are easy to extends and re-use.<br>
+          I decided to use storybook to show the progress and got feedback from the UX team.<br>
+          On the other hand, I mainly used react and styled-components libraries. I dockerize the app to ease the deploy job to the client\'s developers.`,
+          image: '/images/calendar.png',
+          className: '',
+        },
+        {
+          title: 'A bit of code',
+          description: '',
+          image: '/images/code-inputfield.png',
+          className: CUSTOM_SECTION_1,
         }
       ]
     }
