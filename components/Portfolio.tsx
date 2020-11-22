2cee998079f4d18c6c80f484b89d57e48d8c96ff
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-22 17:07:32 -0300

    WIP portfolio page

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,17 +1,38 @@
+import Link from 'next/link';
+import parse from 'html-react-parser';
 import {
+  BOLD,
   LINK,
   PROJECTS,
   TAKE_A_LOOK,
+  CUSTOM_PROJECT_LINK,
   CUSTOM_PORTFOLIO_LINK,
   SECOND_PORTFOLIO_SECTION,
 } from '../utils/constants';
 import {
   Description,
+  ProjectTitle,
+  TextContainer,
+  ProjectContainer,
+  ProjectImg as Img,
+  ProjectDescription,
   DescriptionContainer,
   PortfolioHeader as Header,
   PortfolioSection as Section,
 } from './styles/Portfolio.styles';
 import Action from './Action';
+import { Card } from './UI';
+
+const getTechnologies = ({ title, list }: Technologies) => (
+  <>
+    <p className={BOLD}>{title}</p>
+    <ul>
+      {list.map(tech => (
+        <li>{tech}</li>
+      ))}
+    </ul>
+  </>
+);
 
 const Portfolio = ({
   title,
@@ -33,16 +54,25 @@ const Portfolio = ({
       </DescriptionContainer>
     </Section>
     <Section className={SECOND_PORTFOLIO_SECTION} id={PROJECTS}>
-      <p>{title}</p>
-      <p>{description}</p>
-      <ul>
-        {projects.map(project => (
-          <li>
-            <p>{project.title}</p>
-            <p>{project.description}</p>
-          </li>
-        ))}
-      </ul>
+      {projects.map(project => (
+        <Card>
+          <TextContainer>
+            <ProjectContainer>
+              <Action
+                text={project.title}
+                type={LINK}
+                href={`/portfolio/${project.id}`}
+                className={CUSTOM_PROJECT_LINK}
+              />
+              <ProjectDescription>{parse(project.description)}</ProjectDescription>
+              {getTechnologies(project.technologies)}
+            </ProjectContainer>
+            <Link href={`/portfolio/${project.id}`}>
+              <Img src={project.image} alt={project.alt} />
+            </Link>
+          </TextContainer>
+        </Card>
+      ))}
     </Section>
   </>
 );
@@ -51,10 +81,20 @@ interface Section {
   title: string
   description: string
   image: string
+  id?: string
+  alt?: string
+  className?: string
 }
 
-interface Project extends Section {
+interface Technologies {
+  title: string
+  list: string[]
+}
+
+export interface Project extends Section {
   sections: Section[]
+  technologies: Technologies
+  className?: string
 }
 
 export interface PortfolioProps {
--- components/styles/Portfolio.styles.tsx
+++ components/styles/Portfolio.styles.tsx
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
 import { ResumeSection } from './Resume.styles';
 import { Header } from './About.styles';
+import { CUSTOM_PROJECT_LINK, CUSTOM_PORTFOLIO_LINK, CUSTOM_DESCRIPTION_CONTAINER } from '../../utils/constants';
+import { Img } from '../UI/styles/Img.styles';
 
 export const Container = styled.div``;
 
-export const PortfolioSection = styled(ResumeSection)``;
+export const PortfolioSection = styled(ResumeSection)`
+  @media (min-width: 768px) {
+    align-items: flex-end;
+  }
+`;
 
 export const PortfolioHeader = styled(Header)``;
 
 export const Description = styled.p`
   @media (min-width: 768px) {
-    background-color: ${({ theme }) => theme.white.w08};
+    // background-color: ${({ theme }) => theme.white.w08};
     color: ${({ theme }) => theme.black.b08};
-    padding: 20px;
-    border-radius: 3px;
-    max-width: 50%;
+    // padding: 20px;
+    // border-radius: 3px;
+    max-width: 75%;
   }
 `;
 
@@ -22,9 +28,67 @@ export const DescriptionContainer = styled.div`
   @media (min-width: 768px) {
     display: flex;
     align-items: center;
-    .custom-portfolio-link {
+    background-color: ${({ theme }) => theme.white.w08};
+    padding: 20px;
+    border-radius: 3px;
+    max-width: 50%;
+    margin: 24px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    .${CUSTOM_PORTFOLIO_LINK} {
+      margin-left: 16px;
       align-self: unset;
-      margin-left: 50px;
+      a {
+        padding: 0;
+        color: ${({ theme }) => theme.orange.o40};
+      }
     }
+
+    &.${CUSTOM_DESCRIPTION_CONTAINER} {
+      max-width: unset;
+      background-color: ${({ theme }) => theme.black.b01};
+    }
+  }
+`;
+
+export const TextContainer = styled.div`
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const ProjectTitle = styled.p`
+  text-transform: uppercase;
+  font-size: 22px;
+  font-weight: 900;
+`;
+
+export const ProjectDescription = styled.p`
+  
+`;
+
+export const ProjectContainer = styled.div`
+  @media (min-width: 768px) {
+    .${CUSTOM_PROJECT_LINK} {
+      a {
+        font-weight: 900;
+        font-size: 22px;
+        margin: 16px 0;
+        padding: 0;
+        color: ${({ theme }) => theme.orange.o40};
+        &:hover {
+          color: ${({ theme }) => theme.orange.o50};
+        }
+      }
+    }
+  }
+`;
+
+export const ProjectImg = styled(Img)`
+  &:hover {
+    cursor: pointer;
   }
 `;
