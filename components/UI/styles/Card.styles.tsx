36918b5386f166dc2aefe89afc4d67c534f0962f
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-11-22 17:09:23 -0300

    Create Card component

---
+++ components/UI/styles/Card.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+  box-shadow: 0 0 3px 3px ${({ theme }) => theme.black.b01};
+  border-radius: 3px;
+`;
